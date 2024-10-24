8882045c370ddb64b208ae2406c6a68fbc40153b
khoa <128579628+vakhoa4875@users.noreply.github.com>
2024-10-24 22:16:03 +0700

    fix: logo + icon + title

--- src/layout/Nav&Footer/components/SearchBar.tsx
+++ src/layout/Nav&Footer/components/SearchBar.tsx
@@ -15,6 +15,8 @@ export const SearchBar = () => {
     { label: "Discover", value: "1", link: "/" },
     { label: "Browse", value: "2", link: "/browser" },
     { label: "News", value: "3", link: "/news" },
+    { label: "Wishlist", value: "4", link: "/wishlist" },
+    { label: "Cart", value: "5", link: "/cart" },
   ];
   const handleAddToCart = () => {
     const item: CartItem = { slug: "dying-light-2-stay-human", quantity: 3 };
@@ -30,16 +32,16 @@ export const SearchBar = () => {
   return (
     <>
       <div className="sticky top-0 z-50 mx-auto h-[100px] w-11/12 bg-bgMainColor lg:w-3/4">
-        <div className="flex h-full items-center justify-between">
+        <div className="flex items-center justify-between h-full">
           <div className="flex items-center">
             <div className="relative rounded-full bg-gray300">
-              <i className="pi pi-search absolute left-3 top-1/2 -translate-y-1/2 transform text-gray100"></i>
+              <i className="absolute transform -translate-y-1/2 pi pi-search left-3 top-1/2 text-gray100"></i>
               <InputText
                 placeholder="Search store"
                 className="w-[230px] bg-transparent p-3 pl-10 text-xs text-white focus:ring-0"
               />
             </div>
-            <div className="ml-5 hidden lg:block">
+            <div className="hidden ml-5 lg:block">
               <ul className="flex items-center gap-8 text-sm font-normal text-gray150">
                 <li className="">
                   <NavLink
@@ -73,7 +75,7 @@ export const SearchBar = () => {
                 </li>
               </ul>
             </div>
-            <div className="ml-5 block lg:hidden">
+            <div className="block ml-5 lg:hidden">
               <Dropdown
                 value={selectedItem}
                 options={options}
@@ -98,7 +100,7 @@ export const SearchBar = () => {
             </ul>
           </div>
           {/* Cái này cũg thế kô rõ thì lên trên epic game xem */}
-          <div className="">
+          <div className="hidden lg:block">
             <ul className="flex items-center gap-8 text-sm font-normal text-gray150">
               <li className="text-white">Wishlist</li>
               <li className="hover:text-white">
