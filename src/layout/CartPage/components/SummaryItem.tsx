3e2f8c33e63f1095fbe2e7dc9f04809559b2d8de
khoa <128579628+vakhoa4875@users.noreply.github.com>
2024-10-28 16:19:14 +0700

    feat!: Cart + Checkout + Send license keys

--- src/layout/CartPage/components/SummaryItem.tsx
+++ src/layout/CartPage/components/SummaryItem.tsx
@@ -17,16 +17,16 @@ export const SummaryItem: React.FC<SummaryItemProps> = ({
   quantity,
 }) => {
   return (
-    <div className="flex max-w-sm items-center py-4">
+    <div className="flex items-center max-w-sm py-4">
       {/* Image */}
       <img
         src={imageUrl}
         alt={title}
-        className="mr-4 h-28 w-20 rounded-md object-cover"
+        className="object-cover w-20 mr-4 rounded-md h-28"
       />
 
       {/* Game Info */}
-      <div>
+      <div className="w-full">
         <h3 className="text-lg font-bold text-gray-900">{title}</h3>
         <p className="text-sm text-gray-500">{publisher}</p>
         <div className="flex items-center justify-between">
