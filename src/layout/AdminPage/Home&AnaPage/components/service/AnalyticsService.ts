729391d3381fbce022a8f7ccac7362cd8ddd30b0
chauphat <hieuphung2111@gmail.com>
2024-12-17 20:54:53 +0700

    feat: fix ui home admin

--- src/layout/AdminPage/Home&AnaPage/components/service/AnalyticsService.ts
+++ src/layout/AdminPage/Home&AnaPage/components/service/AnalyticsService.ts
@@ -9,3 +9,21 @@ export const getAnalytics = async (): Promise<Analytics> => {
     throw new Error("Failed to fetch analytics summary");
   }
 };
+
+export const getRevenueVsProfit = async () => {
+  const response = await apiClient.get("/api/analytics/revenue-vs-profit");
+  if (response.data.status === "OK") {
+    return response.data.data;
+  } else {
+    throw new Error("Failed to fetch revenue vs profit data");
+  }
+};
+
+export const getMonthlyUserGrowth = async () => {
+  const response = await apiClient.get("/api/analytics/monthly-user-growth");
+  if (response.data.status === "OK") {
+    return response.data.data;
+  } else {
+    throw new Error("Failed to fetch monthly user growth data");
+  }
+};
